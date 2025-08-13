f9c4c48dc0c298e59737e25a8f67c4cb5710445d
par247 <parantezprojects@gmail.com>
2025-08-13 18:55:26 +0300

    Added llamacpp interactive terminal for vscode

---
+++ .docker/php/bin/llamacpp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+content=$(cat /root/sshpass)
+
+IFS=':' read -r -a parts <<< "$content"
+
+# Değişkenleri tanımlayalım
+HOSTNAME=${parts[0]}
+PASSWORD=${parts[1]}
+
+sshpass -p $PASSWORD ssh $HOSTNAME -t "cd Desktop/synapseui\(synaptic\ framework\) && docker compose exec -it llamacpp sh"

0353b03349b6bdf077b8d9d11463fd0db70a0fad
par247 <parantezprojects@gmail.com>
2025-08-20 18:28:37 +0300

    Added json renderer

--- src/platform/Native/src/Scopes/RenderScope.php
+++ src/platform/Native/src/Scopes/RenderScope.php
@@ -29,6 +29,7 @@ class RenderScope
             'mime' => match ($type)
             {
                 'html' => 'text/html; charset=utf-8',
+                'json' => 'application/json; charset=utf-8',
                 'xml'  => 'application/xml; charset=utf-8',
                 'txt'  => 'text/plain; charset=utf-8',
                 default => throw new InvalidArgumentException("Unsupported render type: {$type}")
